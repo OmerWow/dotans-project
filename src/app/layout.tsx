396f57188172270ad01d925043ef1a6029d9a1ff
OmerWow <omereelbaz3@gmail.com>
2024-06-22 18:25:58 +0300

    chore: change language and direction (hebrew)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { ReactNode } from "react";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -9,10 +10,10 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="he-IL" dir="rtl">
       <body>{children}</body>
     </html>
   );
