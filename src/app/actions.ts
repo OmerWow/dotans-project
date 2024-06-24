05a10706e3d1eab64c55e43899956605afd5d5e3
OmerWow <omereelbaz3@gmail.com>
2024-06-24 16:44:58 +0300

    feat: user login

---
+++ src/app/actions.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import clientPromise from "../../lib/mongodb";
+
+export async function LogUserIn(prevState: any, formData: FormData) {
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    const user = await clientPromise.collection("users").findOne({ email, password });
+
+    if (user) {
+        return redirect("/dashboard/home");
+    }
+
+    return "שם משתמש או סיסמא שגויים";
+}
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,6 +1,11 @@
-import Link from "next/link";
+"use client";
+
+import { LogUserIn } from "@/app/actions";
+import { useActionState } from "react";
 
 export default function SignIn() {
+  const [error, action, isPending] = useActionState(LogUserIn, "");
+
   return (
     <div className="flex h-screen flex-1 flex-col justify-center place-items-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -15,7 +20,7 @@ export default function SignIn() {
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
+        <form className="space-y-6" action={ action }>
           <div>
             <label
               htmlFor="email"
@@ -54,13 +59,19 @@ export default function SignIn() {
             </div>
           </div>
 
-          <div>
-            <Link
-              href="/dashboard/home"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          <div className="flex flex-col place-items-center space-y-4">
+            <button
+              type="submit"
+              disabled={ isPending }
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:bg-gray-500 disabled:cursor-not-allowed disabled:text-gray-50"
             >
-              התחבר
-            </Link>
+              { isPending ? "מתחבר..." : "התחבר" }
+            </button>
+            <div className="min-h-6">
+              { error &&
+                <p className="text-red-500">{ error }</p>
+              }
+            </div>
           </div>
         </form>
       </div>
