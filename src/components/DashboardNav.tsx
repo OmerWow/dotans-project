4b9bf84e453949fe560f458051313e2c165a9d9e
OmerWow <omereelbaz3@gmail.com>
2024-06-28 15:54:37 +0300

    fix: dashboard nav not working

--- src/components/DashboardNav.tsx
+++ src/components/DashboardNav.tsx
@@ -11,6 +11,7 @@ import {
   MenuItems,
 } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const user = {
@@ -40,29 +41,29 @@ export default function DashboardNav() {
   return (
     <div className="min-h-full">
       <Disclosure as="nav" className="bg-gray-800">
-        { ({ open }) => (
+        {({ open }) => (
           <>
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
               <div className="flex h-16 items-center justify-between">
                 <div className="hidden md:block">
                   <div className="ml-10 flex items-baseline space-x-4 space-x-reverse">
-                    { navigation.map((item) => (
-                      <a
-                        key={ item.name }
-                        href={ item.href }
-                        className={ classNames(
+                    {navigation.map((item) => (
+                      <Link
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
                           item.href === pathname
                             ? "bg-gray-900 text-white"
                             : "text-gray-300 hover:bg-gray-700 hover:text-white",
                           "rounded-md px-3 py-2 text-sm font-medium",
-                        ) }
+                        )}
                         aria-current={
                           item.href === pathname ? "page" : undefined
                         }
                       >
-                        { item.name }
-                      </a>
-                    )) }
+                        {item.name}
+                      </Link>
+                    ))}
                   </div>
                 </div>
                 <div className="hidden md:block">
@@ -76,7 +77,7 @@ export default function DashboardNav() {
                       <BellIcon className="h-6 w-6" aria-hidden="true" />
                     </button>
 
-                    {/* Profile dropdown */ }
+                    {/* Profile dropdown */}
                     <Menu as="div" className="relative mr-3">
                       <div>
                         <MenuButton className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
@@ -84,7 +85,7 @@ export default function DashboardNav() {
                           <span className="sr-only">Open user menu</span>
                           <img
                             className="h-8 w-8 rounded-full"
-                            src={ user.imageUrl }
+                            src={user.imageUrl}
                             alt=""
                           />
                         </MenuButton>
@@ -93,35 +94,35 @@ export default function DashboardNav() {
                         transition
                         className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
                       >
-                        { userNavigation.map((item) => (
-                          <MenuItem key={ item.name }>
-                            { ({ focus }) => (
-                              <a
-                                href={ item.href }
-                                className={ classNames(
+                        {userNavigation.map((item) => (
+                          <MenuItem key={item.name}>
+                            {({ focus }) => (
+                              <Link
+                                href={item.href}
+                                className={classNames(
                                   focus ? "bg-gray-100" : "",
                                   "block px-4 py-2 text-sm text-gray-700",
-                                ) }
+                                )}
                               >
-                                { item.name }
-                              </a>
-                            ) }
+                                {item.name}
+                              </Link>
+                            )}
                           </MenuItem>
-                        )) }
+                        ))}
                       </MenuItems>
                     </Menu>
                   </div>
                 </div>
                 <div className="-mr-2 flex md:hidden">
-                  {/* Mobile menu button */ }
+                  {/* Mobile menu button */}
                   <DisclosureButton className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                     <span className="absolute -inset-0.5" />
                     <span className="sr-only">Open main menu</span>
-                    { open ? (
+                    {open ? (
                       <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
                     ) : (
                       <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                    ) }
+                    )}
                   </DisclosureButton>
                 </div>
               </div>
@@ -129,38 +130,38 @@ export default function DashboardNav() {
 
             <DisclosurePanel className="md:hidden">
               <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-                { navigation.map((item) => (
+                {navigation.map((item) => (
                   <DisclosureButton
-                    key={ item.name }
+                    key={item.name}
                     as="a"
-                    href={ item.href }
-                    className={ classNames(
+                    href={item.href}
+                    className={classNames(
                       item.href === pathname
                         ? "bg-gray-900 text-white"
                         : "text-gray-300 hover:bg-gray-700 hover:text-white",
                       "block rounded-md px-3 py-2 text-base font-medium",
-                    ) }
-                    aria-current={ item.href === pathname ? "page" : undefined }
+                    )}
+                    aria-current={item.href === pathname ? "page" : undefined}
                   >
-                    { item.name }
+                    {item.name}
                   </DisclosureButton>
-                )) }
+                ))}
               </div>
               <div className="border-t border-gray-700 pb-3 pt-4">
                 <div className="flex items-center px-5">
                   <div className="flex-shrink-0">
                     <img
                       className="h-10 w-10 rounded-full"
-                      src={ user.imageUrl }
+                      src={user.imageUrl}
                       alt=""
                     />
                   </div>
                   <div className="ml-3">
                     <div className="text-base font-medium leading-none text-white">
-                      { user.name }
+                      {user.name}
                     </div>
                     <div className="text-sm font-medium leading-none text-gray-400">
-                      { user.email }
+                      {user.email}
                     </div>
                   </div>
                   <button
@@ -173,21 +174,21 @@ export default function DashboardNav() {
                   </button>
                 </div>
                 <div className="mt-3 space-y-1 px-2">
-                  { userNavigation.map((item) => (
+                  {userNavigation.map((item) => (
                     <DisclosureButton
-                      key={ item.name }
+                      key={item.name}
                       as="a"
-                      href={ item.href }
+                      href={item.href}
                       className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
                     >
-                      { item.name }
+                      {item.name}
                     </DisclosureButton>
-                  )) }
+                  ))}
                 </div>
               </div>
             </DisclosurePanel>
           </>
-        ) }
+        )}
       </Disclosure>
     </div>
   );
