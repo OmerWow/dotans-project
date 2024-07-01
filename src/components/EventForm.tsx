147e821e206efe0ea87e4df939ba3d2259bababf
OmerWow <omereelbaz3@gmail.com>
2024-07-01 19:20:31 +0300

    chore: remove unused code

--- src/components/EventForm.tsx
+++ src/components/EventForm.tsx
@@ -182,30 +182,6 @@ export default function EventForm({
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
-              {/* <label
-                htmlFor="country"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                מתנדבים
-              </label>
-              <div className="mt-2">
-                <select
-                  id="volunteers"
-                  name="volunteers"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                >
-                  {volunteers.map((volunteer) => {
-                    return (
-                      <option
-                        key={volunteer._id.toString()}
-                        value={volunteer._id.toString()}
-                      >
-                        {volunteer.firstName} {volunteer.lastName}
-                      </option>
-                    );
-                  })}
-                </select>
-              </div> */}
               <fieldset>
                 <legend className="block text-sm font-medium leading-6 text-gray-900">
                   מתנדבים
