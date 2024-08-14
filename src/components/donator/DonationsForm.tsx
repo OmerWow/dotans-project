868aabff5aeca37987367e57302f2cf03c6a2e3b
Omer Elbaz <omereelbaz3@gmail.com>
2024-08-14 16:58:04 +0300

    fix: can't submit donations

--- src/components/donator/DonationsForm.tsx
+++ src/components/donator/DonationsForm.tsx
@@ -69,11 +69,9 @@ export default function DonationsForm({ donations, setDonations }: DonationsForm
                             id="donationValue"
                             name="donationValue"
                             type="number"
-                            step={100}
                             placeholder="0.00"
                             aria-describedby="donationValue"
                             className="block w-full rounded-md border-0 py-1.5 pl-7 pr-12 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            required
                         />
                         <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                             <span id="donationValue" className="text-gray-500 sm:text-xs">
